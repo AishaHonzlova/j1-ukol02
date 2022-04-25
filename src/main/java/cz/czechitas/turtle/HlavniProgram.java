95014f2630dee5a5627b4c8c9bac5cb26318b0c9
AishaHonzlová <honzlova.a@gmail.com>
2022-04-25 15:18:12 +0200

    Zmrzlina

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -8,6 +8,28 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+        nakresliZmrzlinu(zofka);
     }
 
-}
+    private void nakresliTrojuhelnik(Turtle zofka) {
+        zofka.turnRight(160);
+        zofka.move(300);
+        zofka.turnLeft(140);
+        zofka.move(300);
+        zofka.turnLeft(110.0);
+        zofka.move(205);
+        zofka.turnRight(90);
+    }
+
+    private void nakresliKolecko(Turtle zofka) {
+        for(int i = 0; i < 90; i++) {
+            zofka.turnRight(4);
+            zofka.move(7);
+        }
+    }
+
+    private void nakresliZmrzlinu(Turtle zofka) {
+        nakresliTrojuhelnik (zofka);
+        nakresliKolecko(zofka);
+        }
+    }
