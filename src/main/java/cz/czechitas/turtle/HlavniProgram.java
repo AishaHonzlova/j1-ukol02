682e77cf58dc3dd7c04d6f4627e4881bdd33581c
AishaHonzlová <honzlova.a@gmail.com>
2022-04-25 15:24:39 +0200

    Zmrzlina - upraveno→ barva, délka

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,6 +2,8 @@ package cz.czechitas.turtle;
 
 import cz.czechitas.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
 
     public void main(String[] args) {
@@ -12,16 +14,18 @@ public class HlavniProgram {
     }
 
     private void nakresliTrojuhelnik(Turtle zofka) {
+        zofka.setPenColor(Color.ORANGE);
         zofka.turnRight(160);
         zofka.move(300);
         zofka.turnLeft(140);
         zofka.move(300);
         zofka.turnLeft(110.0);
-        zofka.move(205);
+        zofka.move(203);
         zofka.turnRight(90);
     }
 
     private void nakresliKolecko(Turtle zofka) {
+        zofka.setPenColor(Color.PINK);
         for(int i = 0; i < 90; i++) {
             zofka.turnRight(4);
             zofka.move(7);
@@ -33,3 +37,5 @@ public class HlavniProgram {
         nakresliKolecko(zofka);
         }
     }
+
+
