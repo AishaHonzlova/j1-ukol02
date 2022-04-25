bf9bc9ab192b670d79f657ec84a0effc5c549e6c
AishaHonzlová <honzlova.a@gmail.com>
2022-04-25 16:19:37 +0200

    Sněhulák

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -11,9 +11,13 @@ public class HlavniProgram {
 
         zofka = new Turtle();
         nakresliZmrzlinu(zofka);
+        nakresliSnehulaka(zofka);
     }
 
+    //ZMRZLINA
+
     private void nakresliTrojuhelnik(Turtle zofka) {
+        zofka.setLocation(1100, 300);
         zofka.setPenColor(Color.ORANGE);
         zofka.turnRight(160);
         zofka.move(300);
@@ -36,6 +40,56 @@ public class HlavniProgram {
         nakresliTrojuhelnik (zofka);
         nakresliKolecko(zofka);
         }
+
+
+    // SNEHULAK
+
+    private void nakresliHlavu(Turtle zofka) {
+        zofka.setLocation(900,150);
+        zofka.setPenColor(Color.BLUE);
+        for(int i = 0; i < 126; i++) {
+            zofka.turnRight(5);
+            zofka.move(5);
+            }
+        }
+    private void nakresliTelo(Turtle zofka) {
+        for(int i = 0; i < 135; i++) {
+            zofka.turnLeft(4);
+            zofka.move(6);
+        }
+    }
+    private void nakresliNohy(Turtle zofka) {
+        for(int i = 0; i < 90; i++) {
+            zofka.turnRight(4);
+            zofka.move(7);
+        }
+    }
+    private void nakresliRuce(Turtle zofka) {
+        for(int i = 0; i < 25; i++) {
+            zofka.turnLeft(4);
+            zofka.move(6);
+        }
+        for(int i = 0; i < 72.8; i++) {
+            zofka.turnRight(5);
+            zofka.move(2);
+        }
+        zofka.turnLeft(85);
+        zofka.penUp();
+        zofka.move(165);
+        zofka.turnRight(90);
+
+        zofka.penDown();
+        for(int i = 0; i < 72.8; i++) {
+            zofka.turnLeft(5);
+            zofka.move(2);
+        }
     }
 
+    private void nakresliSnehulaka(Turtle zofka) {
+        nakresliHlavu(zofka);
+        nakresliTelo(zofka);
+        nakresliNohy(zofka);
+        nakresliRuce(zofka);
+    }
 
+}
